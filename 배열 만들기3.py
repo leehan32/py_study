06f4daedf5ed172994671c075250bcd15ed06778
gukhan <dkdleldml12@gmail.com>
2024-11-17 13:05:44 +0900

    101

---
+++ 배열 만들기3.py
@@ -0,0 +1,19 @@
+
+
+
+a = [1, 2, 3, 4, 5]
+
+b = [[1, 3], [0, 4]]
+
+
+
+
+
+def solution(arr, intervals):
+    
+    
+    return arr[intervals[0][0]:intervals[0][1]+1] + arr[intervals[1][0]:intervals[1]10]
+
+
+
+return arr[intervals[0][0]:intervals[0][1] + 1] + arr[intervals[1][0]:intervals[1][1] + 1]
